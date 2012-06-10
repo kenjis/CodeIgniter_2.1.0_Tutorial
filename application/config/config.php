de62bb1c53b80a4aa6fd6c7640ea109d251c4514
kenjis <kenji.uui@gmail.com>
2012-06-11 08:29:31 +0900

    CodeIgniter 2.1.0 Tutorial in User Guide

--- application/config/config.php
+++ application/config/config.php
@@ -1,5 +1,8 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
+// set default charset
+ini_set('default_charset', 'UTF-8');
+
 /*
 |--------------------------------------------------------------------------
 | Base Site URL
--- application/config/routes.php
+++ application/config/routes.php
@@ -38,7 +38,11 @@
 |
 */
 
-$route['default_controller'] = "welcome";
+$route['news/create'] = 'news/create';
+$route['news/(:any)'] = 'news/view/$1';
+$route['news'] = 'news';
+$route['(:any)'] = 'pages/view/$1';
+$route['default_controller'] = 'pages/view';
 $route['404_override'] = '';
 
 
